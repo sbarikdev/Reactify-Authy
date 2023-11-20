4f9cf183b7f16699f682de96b2cbe7a6e6ddab0b
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-11-20 07:59:26 +0100

    Update cartReducer.js

--- src/components/Cart/Redux/cartReducer.js
+++ src/components/Cart/Redux/cartReducer.js
@@ -1,26 +1,32 @@
-// reducers/cartReducer.js
-
-import { ADD_TO_CART, REMOVE_FROM_CART } from './cartActions';
-
 const initialState = {
-  cartItems: [],
+  cart: [],
 };
 
-const cartReducer = (state = initialState, action) => {
+function cartReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART:
+      const existingItem = state.cart.find((item) => item.id === action.payload.id);
+      if (existingItem) {
+        return {
+          ...state,
+          cart: state.cart.map((item) => {
+            if (item.id === action.payload.id) {
+              return { ...item, quantity: item.quantity + 1 };
+            }
+            return item;
+          }),
+        };
+      }
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     case REMOVE_FROM_CART:
       return {
         ...state,
-        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
+        cart: state.cart.filter((item) => item.id !== action.payload),
       };
     default:
       return state;
   }
-};
-
-export default cartReducer;
+}
